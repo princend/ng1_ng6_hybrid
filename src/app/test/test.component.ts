bf69f6210a390d58fcba0da2a5dd540bf51698e7
princend <waxdji3g4y3c04@gmail.com>
2020-04-22 15:43:52 +0800

    onchange

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
+export class TestComponent implements OnInit, OnChanges {
 
   title = "Angular"
-
+  changeValue
   @Input() name = '';
   @Output() outputEvent = new EventEmitter();
+
+
   constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.changeValue = `輸入屬性從 ${changes['name'].previousValue}變更為 : ${changes['name'].currentValue}`
+  }
 
   ngOnInit() {
   }
